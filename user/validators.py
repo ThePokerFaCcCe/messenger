d707c1ec78660e27b7a3205c84376230e6e21d35
thepokerfaccce <thepokerfa3e@gmail.com>
2021-12-11 16:38:39 +0330

    :sparkles: Create user model

---
+++ user/validators.py
@@ -0,0 +1,15 @@
+import re
+
+from django.core import validators
+from django.utils.deconstruct import deconstructible
+from django.utils.translation import gettext_lazy as _
+
+
+@deconstructible
+class UsernameValidator(validators.RegexValidator):
+    regex = r'^[A-Za-z]{1,}[_]?[A-Za-z0-9]{1,}$'
+    message = _(
+        'Invalid username. username must start with a-z, A-Z '
+        'and can contain a-z, A-Z, 0-9 and one underscore( _ )'
+    )
+    flags = re.ASCII
