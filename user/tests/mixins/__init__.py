9e4952cfee3d3e0dba624607b31cfbf465c903a8
thepokerfaccce <thepokerfa3e@gmail.com>
2021-12-30 23:30:53 +0330

    :hammer: Improve structure

---
+++ user/tests/mixins/__init__.py
@@ -0,0 +1,1 @@
+from .token_tests import TokenTest
--- user/tests/test_device.py
+++ user/tests/test_device.py
@@ -1,25 +1,12 @@
 from django.test.testcases import TransactionTestCase
 
+from .mixins import TokenTest
 from .utils import create_device
 from user.models import Device
 
 
-class DeviceTest(TransactionTestCase):
+class DeviceTest(TokenTest, TransactionTestCase):
+    model = Device
 
-    def test_token_hashed(self):
-        device = create_device()
-        self.assertNotEqual(device._token_length, len(device.token))
-
-    def test_encrypted_token(self):
-        device = create_device()
-        self.assertIsNotNone(device.encrypted_token)
-        self.assertNotEqual(device.encrypted_token, device.token)
-
-    def test_find_token(self):
-        device = create_device()
-        self.assertEqual(device,
-                         Device.objects.find_token(device.encrypted_token))
-
-        device_2 = create_device()
-        self.assertNotEqual(device_2,
-                            Device.objects.find_token(device.encrypted_token))
+    def create_instance(self):
+        return create_device()
