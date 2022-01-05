49946469bd9513e3e4881f5dc116207dae61ba7f
thepokerfaccce <thepokerfa3e@gmail.com>
2022-01-06 02:07:33 +0330

    :hammer: Add select&prefetch support for `find_token`

--- user/models/manager/encrypted_token.py
+++ user/models/manager/encrypted_token.py
@@ -1,5 +1,5 @@
 
-from typing import Optional
+from typing import Iterable, Optional
 from django.db.models.base import Model
 from django.db.models.manager import Manager
 from django.contrib.auth.hashers import check_password
@@ -8,9 +8,19 @@ from user.models.utils.token import decrypt_token_and_value
 
 
 class EncryptedTokenManager(Manager):
-    def find_token(self, encrypted_token) -> Optional[Model]:
+    def find_token(self, encrypted_token, select: Iterable[str] = None,
+                   prefetch: Iterable[str] = None) -> Optional[Model]:
         """Find object by encrypted token that contains
-        pk and unencrypted token"""
+        pk and unencrypted token
+
+        Parameters
+        ----------
+        `select`: a list of strings that used for `.select_related()`
+
+        `prefetch`: a list of strings that used for `.prefetch_related()`
+        """
+        prefetch = prefetch if prefetch is not None else []
+        select = select if select is not None else []
 
         if encrypted_token is None:
             return None
@@ -21,10 +31,10 @@ class EncryptedTokenManager(Manager):
 
         if value := decrypt_token_and_value(key, encrypted_token):
             instance_id, token = value
-
-            if instance := self.filter(pk=instance_id):
-                instance = instance[0]
-
+            instance = self.filter(pk=instance_id)\
+                .select_related(*select)\
+                .prefetch_related(*prefetch).first()
+            if instance:
                 if self.check_token(token, instance.token):
                     return instance
 
