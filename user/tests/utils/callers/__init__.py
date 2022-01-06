0e284bc1a2f5ea812f312aac77298bbc4447a429
thepokerfaccce <thepokerfa3e@gmail.com>
2022-01-06 22:31:46 +0330

    :heavy_check_mark: Add access view tests

--- user/tests/utils/callers/__init__.py
+++ user/tests/utils/callers/__init__.py
@@ -1,3 +1,10 @@
 from .base_caller import BaseCaller
-from .verifycode_caller import VerifyCodeViewCaller, VERIFYCODE_CREATE_URL, VERIFYCODE_CHECK_URL
-from .device_caller import DeviceViewCaller, device_detail_url, DEVICE_LIST_URL
+from .verifycode_caller import (VerifyCodeViewCaller,
+                                VERIFYCODE_CREATE_URL,
+                                VERIFYCODE_CHECK_URL)
+from .device_caller import (DeviceViewCaller,
+                            device_detail_url,
+                            DEVICE_LIST_URL)
+from .access_caller import (AccessViewCaller,
+                            access_detail_url,
+                            ACCESS_LIST_URL)
