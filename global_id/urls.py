a30173bc60b078923966af39a44709698f1e132b
ThePokerFaCcCe <thepokerfa3e@gmail.com>
2022-02-07 02:01:30 +0330

    :hammer: Add app_name for guid urls

--- global_id/urls.py
+++ global_id/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
+from .apps import GlobalIdConfig
 from global_id.views import GUIDViewset
+
+app_name = GlobalIdConfig.name
 
 router = DefaultRouter()
 router.register('', GUIDViewset, 'guid')
