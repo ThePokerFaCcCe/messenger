10ca39ea53a4ed899f20af339be42448a188849c
ThePokerFaCcCe <thepokerfa3e@gmail.com>
2022-02-09 15:59:54 +0330

    :hammer: Improve `check_task_failed`

--- core/utils/tasks_helper.py
+++ core/utils/tasks_helper.py
@@ -1,9 +1,10 @@
 def check_task_failed(task, raise_exception=False,
                       delete_instances_on_fail=[]):
+    result = task.get(propagate=False)
     failed = task.failed()
     if failed:
         for instance in delete_instances_on_fail:
             instance.delete()
         if raise_exception:
-            raise task
+            raise result
     return failed
