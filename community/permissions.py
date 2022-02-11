39363312f062c1add70b4d4a686adaf33fe1a83a
ThePokerFaCcCe <thepokerfa3e@gmail.com>
2022-02-12 01:31:23 +0330

    :sparkles: Add member permissions

---
+++ community/permissions.py
@@ -0,0 +1,44 @@
+from typing import Optional
+from rest_framework import permissions
+
+from community.models import Member
+
+
+def get_member(request, view) -> Optional[Member]:
+    user_id = request.user.pk
+
+    cid_lookup_url = getattr(
+        view, 'nested_lookup_field',
+        getattr(view, 'lookup_url_kwarg', None)
+        or
+        getattr(view, 'lookup_field')
+    )
+    community_id = view.kwargs.get(cid_lookup_url)
+
+    return Member.objects.filter(
+        community_id=community_id, user_id=user_id
+    ).first()
+
+
+class IsCommunityOwnerMember(permissions.IsAuthenticated):
+    def has_permission(self, request, view):
+        if super().has_permission(request, view):
+            member = get_member(request, view)
+            return member and member.rank == Member.RankChoices.OWNER
+
+
+class IsCommunityAdminMember(permissions.IsAuthenticated):
+    def has_permission(self, request, view):
+        if super().has_permission(request, view):
+            member = get_member(request, view)
+            return member and member.rank >= Member.RankChoices.ADMIN
+
+
+class IsCommunityNormalMember(permissions.IsAuthenticated):
+    def has_permission(self, request, view):
+        if super().has_permission(request, view):
+            member = get_member(request, view)
+            print(member)
+            print(member.rank)
+            print(member.rank >= 2)
+            return member and member.rank >= Member.RankChoices.NORMAL
