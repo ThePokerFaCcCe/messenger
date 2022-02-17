ae467922ca630a73f775e2bb3e278e561298c270
ThePokerFaCcCe <thepokerfa3e@gmail.com>
2022-02-17 21:40:06 +0330

    :sparkles: Add delete community&member signal handlers

--- community/signals.py
+++ community/signals.py
@@ -1,10 +1,12 @@
 from django.dispatch import receiver
-from django.db.models.signals import post_save
+from django.db import transaction
+from django.db.models.signals import post_save, post_delete
 
 from core.utils import delete_instance_on_error
+from core.signals import post_soft_delete
 from community.models import CommunityChat, Member
-from community.tasks import create_member
-from conversation.tasks import create_conversation
+from community.tasks import create_member, delete_community_members
+from conversation.tasks import create_conversation, delete_conversation
 
 
 # from core.utils import check_task_failed
@@ -35,3 +37,21 @@ def create_member_conversation(sender, instance: Member,
             chat=instance.community,
             user_id=instance.user.pk
         )
+
+
+@transaction.atomic
+@receiver(post_delete, sender=Member)
+def delete_member_conversation(sender, instance: Member, **kwargs):
+    delete_conversation(chat_id=instance.community_id,
+                        user_id=instance.user_id)
+    if (instance.rank == Member.RankChoices.OWNER
+            and not instance.community.is_deleted
+        ):
+        instance.community.soft_delete()
+
+
+@transaction.atomic
+@receiver([post_soft_delete, post_delete], sender=CommunityChat)
+def delete_conversation_members(sender, instance: CommunityChat, **kwargs):
+    uids = delete_community_members(community_id=instance.pk)
+    delete_conversation(chat_id=instance.pk, user_id=uids)
