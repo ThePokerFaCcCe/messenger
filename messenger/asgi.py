227e272594911d40dc5c5ebd974e576e5871939e
ThePokerFaCcCe <thepokerfa3e@gmail.com>
2022-02-24 21:01:20 +0330

    :wrench: Add asgi configs

--- messenger/asgi.py
+++ messenger/asgi.py
@@ -1,16 +1,18 @@
-"""
-ASGI config for messenger project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
-"""
-
 import os
-
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messenger.settings')
+from . import routing
 
-application = get_asgi_application()
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "messenger.settings")
+
+websocket_application = AuthMiddlewareStack(
+    URLRouter(routing.websocket_urlpatterns)
+)
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": websocket_application,
+})
