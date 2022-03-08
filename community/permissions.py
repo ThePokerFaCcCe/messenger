35fb15685642dad43149a3f153aab961e0bc1eb5
ThePokerFaCcCe <thepokerfa3e@gmail.com>
2022-03-08 19:57:08 +0330

    :hammer: Add consumer support for member permissions

--- community/permissions.py
+++ community/permissions.py
@@ -1,19 +1,23 @@
 from typing import Optional
-from rest_framework import permissions
+from rest_framework import permissions, views
 
 from community.models import Member
 
 
 def get_member(request, view) -> Optional[Member]:
     user_id = request.user.pk
-
-    cid_lookup_url = getattr(
-        view, 'nested_lookup_field',
-        getattr(view, 'lookup_url_kwarg', None)
-        or
-        getattr(view, 'lookup_field')
-    )
-    community_id = view.kwargs.get(cid_lookup_url)
+    if isinstance(view, views.APIView):
+        cid_lookup_url = getattr(
+            view, 'nested_lookup_field',
+            getattr(view, 'lookup_url_kwarg', None)
+            or
+            getattr(view, 'lookup_field')
+        )
+        community_id = view.kwargs.get(cid_lookup_url)
+    else:
+        # Probably the view is a Consumer object
+        community_lookup = getattr(view, 'community_query_lookup', 'chat_id')
+        community_id = request.query.get(community_lookup)
 
     return Member.objects.filter(
         community_id=community_id, user_id=user_id
@@ -33,10 +37,16 @@ class IsCommunityOwnerMember(MemberRankPermissionMixin):
     def check_rank(self, rank) -> bool:
         return rank == Member.RankChoices.OWNER
 
+    def has_object_permission(self, request, view, obj):
+        return self.has_permission(request, view)
+
 
 class IsCommunityAdminMember(MemberRankPermissionMixin):
     def check_rank(self, rank) -> bool:
         return rank >= Member.RankChoices.ADMIN
+
+    def has_object_permission(self, request, view, obj):
+        return self.has_permission(request, view)
 
 
 class IsCommunityNormalMember(MemberRankPermissionMixin):
