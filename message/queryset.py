50393f32c008f1183e2fccdf72c8f8cb142ac5bb
ThePokerFaCcCe <thepokerfa3e@gmail.com>
2022-03-08 17:31:40 +0330

    :sparkles: Add `delete_message` query function

--- message/queryset.py
+++ message/queryset.py
@@ -1,8 +1,8 @@
-from django.db.models.expressions import Q
+from django.db.models import Q, QuerySet
 from message.models import Message, DeletedMessage
 
 
-def get_chat_messages(chat_id, user_id):
+def get_chat_messages(chat_id, user_id) -> QuerySet:
     """
     returns a queryset of messages that
     isn't deleted by user or others
@@ -15,4 +15,14 @@ def get_chat_messages(chat_id, user_id):
     return Message.objects.filter_not_deleted(
         ~Q(id__in=deleted_msgs),
         chat_id=chat_id,
+    )
+
+
+def delete_message(msg_id, user_id) -> DeletedMessage:
+    """
+    deletes a message for specified user and
+    returns DeletedMessage instance
+    """
+    return DeletedMessage.objects.create(
+        user_id=user_id, message_id=msg_id
     )
