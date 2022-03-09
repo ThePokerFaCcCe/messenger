1eb3755f8c3caae2a7c759fdba8873dde496af3b
ThePokerFaCcCe <thepokerfa3e@gmail.com>
2022-03-09 16:44:37 +0330

    :hammer: Add update_message signals

--- messenger_channels/signals/message_signals.py
+++ messenger_channels/signals/message_signals.py
@@ -1,10 +1,15 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
+from django.dispatch import Signal
 
 from core.signals import post_soft_delete
 from message.models import Message, DeletedMessage
 from message.serializers import MessageSerializer, DeletedMessageSerializer, HardDeletedMessageSerializer
 from messenger_channels.utils import send_message_event
+
+
+pre_update_message = Signal(providing_args=['instance'])
+post_update_message = Signal(providing_args=['instance'])
 
 
 @receiver(post_save, sender=Message)
@@ -19,9 +24,9 @@ def send_message_to_channels(sender, instance: Message,
 
 
 @receiver(post_save, sender=DeletedMessage)
-def send_delete_message_from_channels(sender,
-                                      instance: DeletedMessage,
-                                      created, **kwargs):
+def send_delete_message_to_channels(sender,
+                                    instance: DeletedMessage,
+                                    created, **kwargs):
     send_message_event(
         group_name=f"user_{instance.user_id}",
         event_title="delete_message",
@@ -30,10 +35,25 @@ def send_delete_message_from_channels(sender,
 
 
 @receiver([post_soft_delete, post_delete], sender=Message)
-def send_hard_delete_message_from_channels(sender, instance: Message,
-                                           **kwargs):
+def send_hard_delete_message_to_channels(sender, instance: Message, **_):
     send_message_event(
         group_name=instance.chat_id,
         event_title="hard_delete_message",
         message=HardDeletedMessageSerializer(instance).data
     )
+
+
+@receiver(post_update_message, sender=Message)
+def send_update_message_to_channels(sender, instance: Message, **_):
+    send_message_event(
+        group_name=instance.chat_id,
+        event_title="update_message",
+        message=MessageSerializer(instance).data
+    )
+
+
+@receiver(post_update_message, sender=Message)
+def set_message_is_edited(sender, instance: Message, **_):
+    if not instance.is_edited:
+        instance.is_edited = True
+        instance.save()
