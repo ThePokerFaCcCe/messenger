f8c6d38ee6183d0ff9d609fdd39822f709ec0b84
ThePokerFaCcCe <thepokerfa3e@gmail.com>
2022-03-09 17:55:48 +0330

    :hammer: Improve code & add `send_event` func

--- messenger_channels/utils.py
+++ messenger_channels/utils.py
@@ -4,16 +4,30 @@ from channels.layers import get_channel_layer
 layer = get_channel_layer()
 
 
-def send_message_event(group_name, event_title, **data):
-    """
-    Calls `event_send_message` method of channels
-    that joined in `group_name`
-    """
+def send_event(group_name, event_title: str,
+               event_type: str, **data):
+    '''
+    Calls `event_type` method of channels in `group_name`
+
+    PS: "event." will add to `event_type` if not exists.
+    e.g. "send" -> "event.send"
+    '''
+    event_type = (event_type
+                  if event_type.lower().startswith('event')
+                  else f"event.{event_type}")
     async_to_sync(layer.group_send)(
         str(group_name),
         {
-            'type': "event.send_message",
+            'type': event_type,
             'event': event_title,
             **data
         }
     )
+
+
+def send_message_event(group_name, event_title: str, **data):
+    """
+    Calls `event_send_message` method of channels in `group_name`
+    """
+    send_event(group_name, event_title,
+               event_type="send_message", **data)
