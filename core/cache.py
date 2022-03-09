c959a616221ff2555162c367c86cf14f26775d61
ThePokerFaCcCe <thepokerfa3e@gmail.com>
2022-03-09 20:07:20 +0330

    :sparkles: Add `AppCache` class

---
+++ core/cache.py
@@ -0,0 +1,11 @@
+from cache_helper.cache import Cache
+
+
+class AppCache(Cache):
+    key_patterns = {
+        "deleted_message": "deleted_{msg_id}_{user_id}",
+        "pv_id": "pv_{}_{}",
+    }
+
+
+cache = AppCache()
