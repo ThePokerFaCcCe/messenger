c325c60288d78fea7feed407d2ff17ec858f16a2
ThePokerFaCcCe <thepokerfa3e@gmail.com>
2022-03-14 13:07:30 +0330

    :hammer: Add "user_pvs" cache key

--- core/cache.py
+++ core/cache.py
@@ -5,6 +5,7 @@ class AppCache(Cache):
     key_patterns = {
         "deleted_message": "deleted_{msg_id}_{user_id}",
         "pv_id": "pv_{}_{}",
+        "user_pvs": "pvs_{}",
         "guid": "guid_{}",
     }
 
