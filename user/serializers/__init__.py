336c0213e985ba84db3c686bc870d6abcb3fb163
ThePokerFaCcCe <thepokerfa3e@gmail.com>
2022-04-02 03:57:20 +0430

    :sparkles: Add `UserInfoSerializer`

--- user/serializers/__init__.py
+++ user/serializers/__init__.py
@@ -1,6 +1,7 @@
 from .device_serializers import (DeviceCreateSerializer,
                                  DeviceInfoSerializer)
-from .user_serializers import (UserSerializer,
+from .user_serializers import (UserUpdateSerializer,
+                               UserInfoSerializer,
                                UserStaffUpdateSerializer,
                                UserLastSeenSerializer,
                                UserProfileSerializer)
--- user/serializers/user_serializers.py
+++ user/serializers/user_serializers.py
@@ -1,10 +1,58 @@
 from rest_framework import serializers
 
+from core.utils import get_context_user
 from picturic.serializer_fields import PictureField
 from user.models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserInfoSerializer(serializers.ModelSerializer):
+    profile_image = PictureField(read_only=True)
+    is_contact = serializers.BooleanField(default=False, read_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'first_name',
+            'last_name',
+            'bio',
+            'full_name',
+            'guid',
+            'profile_image',
+            'last_seen',
+            'is_contact',
+            'is_online',
+            'is_staff',
+            'is_scam',
+            'type',
+        ]
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+
+        user = get_context_user(self.context)
+        if (not (user and hasattr(user, 'contacts'))
+                or instance == user):
+            return data
+
+        contact_filter = list(filter(
+            lambda c: c.contacted_user_id == instance.pk,
+            user.contacts.all()
+        ))
+
+        if not contact_filter:
+            return data
+
+        contact = contact_filter[0]
+        data['first_name'] = contact.first_name
+        data['last_name'] = contact.last_name
+        data['full_name'] = contact.full_name
+        data['is_contact'] = True
+
+        return data
+
+
+class UserUpdateSerializer(serializers.ModelSerializer):
     profile_image = PictureField(read_only=True)
 
     class Meta:
