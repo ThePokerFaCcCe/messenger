9684839f1c68ad68ebb7fbe5c97b82d7c92fabbb
ThePokerFaCcCe <thepokerfa3e@gmail.com>
2022-04-25 00:34:38 +0430

    Add `create_contact` function

--- user/tests/utils/creators.py
+++ user/tests/utils/creators.py
@@ -1,9 +1,9 @@
 from core.utils import generate_email
-from user.models import Device, User
+from user.models import Device, User, Contact
 
 
 def create_user(email=None, **kwargs) -> User:
-    email = generate_email() if not email else email
+    email = email or generate_email()
     return User.objects.create_user(email, **kwargs)
 
 
@@ -13,5 +13,13 @@ def create_active_user(**kwargs) -> User:
 
 def create_device(user=None, type=Device.TypeChoices.WINDOWS,
                   model='10 Pro', **kwargs) -> Device:
-    user = create_user() if not user else user
+    user = user or create_user()
     return Device.objects.create(user=user, type=type, model=model, **kwargs)
+
+
+def create_contact(user=None, contacted_user=None, first_name='name', **kwargs) -> Contact:
+    return Contact.objects.create(
+        first_name=first_name,
+        user=user or create_user(),
+        contacted_user=contacted_user or create_user(),
+        **kwargs)
